f393a785d281b8b5d87cf227566d67d0b4e83a3c
Syam Haque <msyamhaque@gmail.com>
2017-11-03 17:50:10 -0700

    add attack

---
+++ Assets/Scripts/robotattack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotattack : MonoBehaviour {
+
+    private bool attacking = false;
+    private float attackTimer = 0;
+    private float attackCooldown = 0.3f;
+
+    public Collider2D attackcol;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        attackcol.enabled = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("f") && !attacking)
+        {
+            attacking = true;
+            attackTimer = attackCooldown;
+            attackcol.enabled = true;
+        }
+
+        if (attacking)
+        {
+            if (attackTimer > 0)
+            {
+                attackTimer -= Time.deltaTime;
+            }
+            else
+            {
+                attacking = false;
+                attackcol.enabled = false;
+            }
+        }
+
+        anim.SetBool("attack", attacking);
+
+    }
+    
+}
